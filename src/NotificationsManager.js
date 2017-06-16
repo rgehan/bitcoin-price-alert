3112701a60b7b58cb385bfa9e7dc4b91095a1c31
Renan GEHAN <rgehan94@gmail.com>
2017-06-16 22:07:25 +0200

    Put config into .env file

--- src/NotificationsManager.js
+++ src/NotificationsManager.js
@@ -3,28 +3,28 @@
 import { PushedAPI } from './api';
 import config from './config';
 
-// Builds a message for a price event
-const getMessage = (direction, threshold, current) => {
-  if(direction == 'rise')
-    return `Valeur du BTC au dessus de ${threshold} (courant: ${current})`;
-  else
-    return `Valeur du BTC au dessous de ${threshold} (courant: ${current})`;  
-}
-
 class NotificationsManager {
+
+  /**
+   * Initialize the instance
+   */
   constructor() {
+    // The list of the threshold we can cross
     this.thresholds = [];
 
+    // Last notification times for the different events
     this.lastNotification = {rise: 0, fall: 0};
 
-    // Build the Push API
+    // Build the Pusher API
     this.api = new PushedAPI({
-      key: 'mf4KnNBk77NLERaZEvNI',
-      secret: '05uQSpqCd9mpnFZ2ronq2MGSJyQ2v3qYrS2f0JqUFFPHGZT0V2kyY54d6EP3FmS4'
+      key: config.pushed.key,
+      secret: config.pushed.secret,
     });
   }
 
-  // Adds a threshold to the list
+  /**
+   * Add a threshold to the list
+   */
   addThreshold(direction, threshold) {
     this.thresholds.push({
       direction,
@@ -32,7 +32,9 @@ class NotificationsManager {
     });
   }
 
-  // On price change, check if a threshold is crossed
+  /**
+   * On price change, check if a threshold is crossed
+   */
   handlePriceChange(current, delta) {
     this.thresholds.forEach(({direction, threshold}) => {
       if(this.thresholdCrossed(direction, threshold, delta, current))
@@ -40,26 +42,35 @@ class NotificationsManager {
     });
   }
 
-  // Return whether or not the user should be notified
+  /**
+   * Return whether or not the user should be notified
+   */
   thresholdCrossed(direction, threshold, delta, current) {
-    return delta > 0 && direction == 'rise' && current > threshold || 
+    return delta > 0 && direction == 'rise' && current > threshold ||
            delta < 0 && direction == 'fall' && current < threshold;
   }
 
-  // Attempts to notify the user
+  /**
+   * Attempts to notify the user
+   */
   attemptNotify(direction, threshold, current) {
     let delta = +(new Date) - this.lastNotification[direction];
 
     // If enough time has passed, notify
     if(delta > config.delay_between_same_notification) {
-      console.log('Enough time has passed, notifying !');
-
-      this.api.send(getMessage(direction, threshold, current));
+      this.api.send(this.buildEventMessage(direction, threshold, current));
       this.lastNotification[direction] = +(new Date);
-    } else {
-      let cooldown = Math.floor((config.delay_between_same_notification - (+new Date - this.lastNotification[direction])) / 1000);
-      console.log(`Not enough time has passed, no notification (cooldown: ${cooldown} sec)`);
     }
+  }
+
+  /**
+   * Build a message for a price event
+   */
+  buildEventMessage(direction, threshold, current) {
+    if(direction == 'rise')
+      return `Valeur du BTC au dessus de ${threshold} (courant: ${current})`;
+    else if(direction == 'fall')
+      return `Valeur du BTC au dessous de ${threshold} (courant: ${current})`;
   }
 }
 
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,10 @@
 // config/index.js
 
+import dotenv from 'dotenv';
+
+// Load the .env config file
+dotenv.config();
+
 const config = () => {
   let refresh_per_hour = 500;
 
@@ -7,6 +12,11 @@ const config = () => {
     refresh_per_hour,
     refresh_delay: 3600 * 1000 / refresh_per_hour,
     delay_between_same_notification: 5 * 60 * 1000, // 5 minutes
+
+    pushed: {
+      key: process.env.PUSHED_KEY,
+      secret: process.env.PUSHED_SECRET,
+    },
   };
 }
 
